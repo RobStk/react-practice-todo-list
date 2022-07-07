37f0a85fd1aecf02e09cafcf11a02b502aea1042
robster <99953834+RobStk@users.noreply.github.com>
2022-07-07 09:11:56 +0200

    basic configuration and react implementation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const rootElement = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<h1>Hello World!</h1>);
