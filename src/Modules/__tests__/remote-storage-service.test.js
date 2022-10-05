21b2526ab9bdd5044ab3db8f70c97f894840b94a
robster <99953834+RobStk@users.noreply.github.com>
2022-10-05 14:08:16 +0200

    RemoteStorageService setData TDD

--- src/Modules/__tests__/remote-storage-service.test.js
+++ src/Modules/__tests__/remote-storage-service.test.js
@@ -1,6 +1,6 @@
 import RemoteStorageService from "../remote-storage-service";
 
-const fetchMock = () =>
+const getResolveMock = () =>
     Promise.resolve({
         ok: true,
         json: () => Promise.resolve(
@@ -11,7 +11,8 @@ const fetchMock = () =>
         )
     });
 
-const rejectedFetchMock = () => Promise.reject();
+const setResolveMock = () => Promise.resolve({ ok: true });
+const rejectedMock = () => Promise.reject({ status: "error test" });
 
 const url = "url";
 const remoteService = new RemoteStorageService(url);
@@ -35,7 +36,7 @@ describe("getData method", () => {
     });
 
     it("should call fetch with url", () => {
-        global.fetch = jest.fn(fetchMock);
+        global.fetch = jest.fn(getResolveMock);
 
         remoteService.getData();
         expect.assertions(1);
@@ -43,7 +44,7 @@ describe("getData method", () => {
     });
 
     it("should return data array", async () => {
-        global.fetch = jest.fn(fetchMock);
+        global.fetch = jest.fn(getResolveMock);
 
         const data = await remoteService.getData();
         const dataIsArray = Array.isArray(data);
@@ -57,11 +58,73 @@ describe("getData method", () => {
     });
 
     it("should return null if reject", async () => {
-        global.fetch = jest.fn(rejectedFetchMock);
+        global.fetch = jest.fn(rejectedMock);
 
         const data = await remoteService.getData();
 
         expect.assertions(1);
         expect(data).toBe(null);
+    });
+
+    it("should output an error message to the console", async () => {
+        global.fetch = jest.fn(rejectedMock);
+        global.console.error = jest.fn();
+        const errorMsg = "Http error: error test";
+
+        await remoteService.getData();
+
+        expect.assertions(1);
+        expect(console.error).toBeCalledWith(errorMsg);
+    })
+});
+
+describe("setData method", () => {
+    const data = [
+        { id: 1, content: "content1" },
+        { id: 2, content: "content2" }
+    ];
+
+    const options = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.resetAllMocks();
+        jest.restoreAllMocks();
+    });
+
+    it("should call fetch with url and correct options object", () => {
+        global.fetch = jest.fn();
+        remoteService.setData(data);
+        expect.assertions(1);
+        expect(fetch).toBeCalledWith(url, options);
+    });
+
+    it("should return true if resolve", async () => {
+        global.fetch = jest.fn(setResolveMock);
+        const response = await remoteService.setData();
+        expect.assertions(1);
+        expect(response).toBe(true);
+    });
+
+    it("should return false if reject", async () => {
+        global.fetch = jest.fn(rejectedMock);
+        const response = await remoteService.setData();
+        expect.assertions(1);
+        expect(response).toBe(false);
+    });
+
+    it("should output an error message to the console", async () => {
+        global.fetch = jest.fn(rejectedMock);
+        global.console.error = jest.fn();
+        const errorMsg = "Http error: error test";
+        await remoteService.setData();
+        expect.assertions(1);
+        expect(console.error).toBeCalledWith(errorMsg);
     });
 });
