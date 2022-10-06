386dfc0965ed7fb0b6e771e6c57075e0da61a86e
robster <99953834+RobStk@users.noreply.github.com>
2022-10-06 12:33:35 +0200

    arrays synchronization

--- src/Modules/__tests__/arrays-synchronizer.test.js
+++ src/Modules/__tests__/arrays-synchronizer.test.js
@@ -5,8 +5,49 @@ const arraySynchronizer = new ArraySynchronizer();
 describe("Interface", () => {
     it("should be implemented", () => {
         const synchronizeIsImplemented = (arraySynchronizer.synchronize !== undefined);
-
         expect.assertions(1);
         expect(synchronizeIsImplemented).toBeTruthy();
+    });
+});
+
+describe("synchronize method", () => {
+    const array1 = [
+        { id: 1, content: "content1", lastUpdate: "20221005053015" },
+        { id: 2, content: "content2", lastUpdate: "20221004063020" },
+        { id: 3, content: "content3 updated", lastUpdate: "20221006053015" },
+        { id: 4, content: "content4", lastUpdate: "20221003204023" },
+        { id: 5, content: "content5", lastUpdate: "20220906053015" },
+        { content: "new content1-6", lastUpdate: "20221006053130", tempId: 1 },
+        { content: "new content1-7", lastUpdate: "20221006053100", tempId: 2 }
+    ];
+    const array2 = [
+        { id: 1, content: "content1", lastUpdate: "20221005053015" },
+        { id: 2, content: "content2", lastUpdate: "20221004063020" },
+        { id: 3, content: "content3", lastUpdate: "20221003052045" },
+        { id: 4, content: "content4 updated", lastUpdate: "20221006053218" },
+        { id: 5, content: "content5", lastUpdate: "20220906053015" },
+        { id: 6, content: "content7", lastUpdate: "20221006052033" },
+        { id: 7, content: "content8", lastUpdate: "20221006051952" }
+    ];
+
+    const array3 = [
+        { id: 4, content: "content4 updated", lastUpdate: "20221006053218" },
+        { content: "new content1-6", lastUpdate: "20221006053130", tempId: 1 },
+        { content: "new content1-7", lastUpdate: "20221006053100", tempId: 2 },
+        { id: 3, content: "content3 updated", lastUpdate: "20221006053015" },
+        { id: 6, content: "content7", lastUpdate: "20221006052033" },
+        { id: 7, content: "content8", lastUpdate: "20221006051952" },
+        { id: 1, content: "content1", lastUpdate: "20221005053015" },
+        { id: 2, content: "content2", lastUpdate: "20221004063020" },
+        { id: 5, content: "content5", lastUpdate: "20220906053015" },
+    ]
+
+    it("should return one array of two", () => {
+        const synchronizedArray = arraySynchronizer.synchronize(array1, array2);
+        expect.assertions(3);
+        const isArray = Array.isArray(synchronizedArray);
+        expect(isArray).toBeTruthy();
+        expect(synchronizedArray).toHaveLength(9);
+        expect(synchronizedArray).toStrictEqual(array3);
     })
 })
--- src/Modules/arrays-synchronizer.js
+++ src/Modules/arrays-synchronizer.js
@@ -27,7 +27,31 @@ class ArraySynchronizer {
     /* ---------------------------------------------------- */
     /* Methods                                              */
     /* ---------------------------------------------------- */
-    #synchronize() { throw new Error("Not implemented method."); }
+
+    /**
+     * Returns an array that is a synchronization of two arrays based on a sync key.
+     * @param {Array} array1 
+     * @param {Array} array2 
+     * @returns {Array}
+     */
+    #synchronize(array1, array2) {
+        const key = this.#syncKey;
+        const sumArray = [...array1, ...array2];
+        sumArray.sort((arr1, arr2) => {
+            return arr2[key] - arr1[key];
+        });
+
+        for (let i = 0; i < sumArray.length; i++) {
+            const element = sumArray[i];
+            const id = element.id;
+            for (let j = i + 1; j < sumArray.length; j++) {
+                const el = sumArray[j];
+                if (el.id && el.id === id) sumArray.splice(j, 1);
+            }
+        }
+
+        return sumArray;
+    }
 }
 
 export default ArraySynchronizer;
