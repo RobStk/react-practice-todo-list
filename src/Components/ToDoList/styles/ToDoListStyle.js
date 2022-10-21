7bda0f866178a6e1ecd9a83c5b71f7372975be47
robster <99953834+RobStk@users.noreply.github.com>
2022-10-21 15:30:30 +0200

    main container's box-shadow

--- src/Components/ToDoList/styles/ToDoListStyle.js
+++ src/Components/ToDoList/styles/ToDoListStyle.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 const ToDoListStyle = styled.div`
     max-width: 1000px;
     margin: 20px auto;
-    padding: 0 20px;
+    padding: 20px;
     display: flex;
     flex-direction: column;
     gap: 10px;
+    -webkit-box-shadow: 0px 0px 30px -10px ${({ theme }) => theme.colors.background.stage1};
+    -moz-box-shadow: 0px 0px 30px -10px ${({ theme }) => theme.colors.background.stage1};
+    box-shadow: 0px 0px 30px -10px ${({ theme }) => theme.colors.background.stage1};
 `
 ToDoListStyle.displayName = "ToDoListStyle";
 
